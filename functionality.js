c5eb8e1a1235b62bd41f50d971b2e76491bbfcd3
wyatwrig <wyatwrig@gmail.com>
2022-02-12 12:57:14 -0700

    added js todos

---
+++ functionality.js
@@ -0,0 +1,20 @@
+// TODO: Write a method to pull urls from currently selected tabs
+
+// On button click
+
+// pull the urls from the currently selected tab
+
+// create a new storage object with the value from the text box as a key and the urls as the value.
+
+// update the storage list html page with a new storage object displaying the name and link shortlist
+
+// TODO: Write a method to load a url storage set
+
+// On click, create tab objects for each url in the storage set with the selected name
+
+// TODO: Write a method to delete a storage object
+
+// On click, remove from storage the selected label and url pair.
+
+
+
