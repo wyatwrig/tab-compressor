29152a074a5dbb19c80713d83b5790ef67894cd4
meilseit <75337068+meilseit@users.noreply.github.com>
2022-02-14 11:24:45 -0700

    added comments

--- list.js
+++ list.js
@@ -1,32 +1,38 @@
  function Populate(data) {
-
-
-    let keyList = Object.keys(data)
-    let table = document.getElementById("table");
+    /*Take one input data from chrome.storage. Uses this
+    information to fill out HTML table.
+     */
+    let keyList = Object.keys(data) //get a list of the dif keys in the data object
+    let table = document.getElementById("table"); //find table node
     let rowCount = table.rows.length;
     for (let i = 0; i < keyList.length; i++) {
-        let row = table.insertRow(rowCount);
-        row.id = i.toString()
+        let row = table.insertRow(rowCount); //insert row that is 3 long
+        row.id = i.toString() // keep track of row by id
         console.log(row.id)
+        //insert cells into row
         row.insertCell(0).innerHTML = keyList[i].toString();
         row.insertCell(1).innerHTML = '<input type="button" value = "Delete" id="delete' + row.id + '">';
         row.insertCell(2).innerHTML = '<input type="button" value = "Restore"  id="">';
-
+        //add event listeners to input node
         row.cells[1].firstChild.addEventListener("click",function(){deleteRow(i)});
         row.cells[2].firstChild.addEventListener("click",function(){restoreTabs(i)});
-
     };
 };
 function loadData(){
+    /* because this program has no local cache,
+    LoadData() takes no inputs and fetches all the
+    user sessions async while also calling populate
+     */
    chrome.storage.sync.get(null, function(data){
        Populate(data);
 
    });
 };
-
-
-
 function testData(){
+    /* A function that takes no inputs for testing purposes.
+    It serves to populate storage enough to ensure features in list HTML
+    are working.
+     */
          chrome.storage.sync.set(
             {
                 biology:
@@ -43,8 +49,10 @@ function testData(){
             },);
 
 };
-
 function deleteRow(i){
+    /*takes input that corresponds to row id.
+    With this information we
+     */
     const row = document.getElementById(i.toString());
     const key = row.cells[0].innerHTML;
     row.remove();
@@ -56,15 +64,11 @@ function restoreTabs(i){
     const key = row.cells[0].innerHTML;
     chrome.storage.sync.get([key], function(data){
         console.log(data)
-        let list = Object.values(data[key]);
-        console.log(list)
+        console.log(data[key])
+        let list = data[key];
         for(let j = 0; j < list.length; j++){
-            console.log(j)
-            console.log(list[j])
             let obj = {url: list[j]};
-            console.log(obj);
-
-            chrome.tabs.create(obj, function(){alert("the tabs where created ")})
+            chrome.tabs.create(obj, function(){alert("the tabs where created ")});
         };
     })
 
@@ -72,6 +76,9 @@ function restoreTabs(i){
 };
 
 
+/*Run these function on window load to fill the HTML page.
+Call the function described above
+ */
 
 
 testData()
