6b700074d5112fa47eb669c5f30f4f7b6b21a1b4
meilseit <75337068+meilseit@users.noreply.github.com>
2022-02-15 12:47:27 -0700

    finished base functionality and added comments to code

--- functionality.js
+++ functionality.js
@@ -1,20 +1,36 @@
-// TODO: Write a method to pull urls from currently selected tabs
+/*Listen for the click of the submit button then process
+the tab information
+ */
+document.getElementById("session-submit").addEventListener("click",function(){
+    let tabs = storeTabs().then((tabs)=>{syncChromeStorage(tabs); }); //take the promise returned form storeTabs wait to fufil then pass on
+    console.log(tabs);
+} );
 
-// On button click
+function storeTabs(){
+    /*take the async API response and store it
+    in a Promise to be resolved
+     */
+   return new Promise((resolve) => {
+       chrome.tabs.query({}, function(tabs){ //aysnc API call for the tab information
+           resolve(tabs)
+       });
+   });
+};
 
-// pull the urls from the currently selected tab
+function syncChromeStorage(tabs){ //this input is a promise that is fulfilled
+    /*Now that the Promise has been fufiled and
+    the values are available, this function
+    will pack the tab information into a session
+     */
+    let urls = [];
+    let obj = {};
+    const sessionName = document.getElementById("session-input").value; // fetch the session input
+    tabs.forEach((tab)=>{urls.push(tab.url)}); //populate the urls
+    obj[sessionName] = urls; //populate the obj
+    chrome.storage.sync.set(obj, function(){}); // store obj in chrome.storage
+};
 
-// create a new storage object with the value from the text box as a key and the urls as the value.
 
-// update the storage list html page with a new storage object displaying the name and link shortlist
-
-// TODO: Write a method to load a url storage set
-
-// On click, create tab objects for each url in the storage set with the selected name
-
-// TODO: Write a method to delete a storage object
-
-// On click, remove from storage the selected label and url pair.
 
 
 
--- list.js
+++ list.js
@@ -1,14 +1,14 @@
- function Populate(data) {
+function Populate(data) {
     /*Take one input data from chrome.storage. Uses this
     information to fill out HTML table.
      */
+     console.log(data)
     let keyList = Object.keys(data) //get a list of the dif keys in the data object
     let table = document.getElementById("table"); //find table node
     let rowCount = table.rows.length;
     for (let i = 0; i < keyList.length; i++) {
         let row = table.insertRow(rowCount); //insert row that is 3 long
         row.id = i.toString() // keep track of row by id
-        console.log(row.id)
         //insert cells into row
         row.insertCell(0).innerHTML = keyList[i].toString();
         row.insertCell(1).innerHTML = '<input type="button" value = "Delete" id="delete' + row.id + '">';
@@ -23,11 +23,12 @@ function loadData(){
     LoadData() takes no inputs and fetches all the
     user sessions async while also calling populate
      */
-   chrome.storage.sync.get(null, function(data){
-       Populate(data);
+        chrome.storage.sync.get(null, function(data){
+            console.log(data);
+            Populate(data);
+        });
+    };
 
-   });
-};
 function testData(){
     /* A function that takes no inputs for testing purposes.
     It serves to populate storage enough to ensure features in list HTML
@@ -56,11 +57,10 @@ function deleteRow(i){
     const row = document.getElementById(i.toString());
     const key = row.cells[0].innerHTML;
     row.remove();
-    chrome.storage.sync.remove([key], function(){alert("removal was a success")})
+    chrome.storage.sync.remove([key], function(){})
 };
 function restoreTabs(i){
     const row = document.getElementById(i.toString());
-    console.log();
     const key = row.cells[0].innerHTML;
     chrome.storage.sync.get([key], function(data){
         console.log(data)
@@ -68,7 +68,7 @@ function restoreTabs(i){
         let list = data[key];
         for(let j = 0; j < list.length; j++){
             let obj = {url: list[j]};
-            chrome.tabs.create(obj, function(){alert("the tabs where created ")});
+            chrome.tabs.create(obj, function(){});
         };
     })
 
@@ -80,8 +80,8 @@ function restoreTabs(i){
 Call the function described above
  */
 
-
-testData()
+testData();
 window.addEventListener('load', event => {
+    alert("now we load data")
     loadData();
-});
+    });
